1ae9c0b21fbd8588fd5670371b64529f68cb21fa
dholi <comunidade@dholi.dev>
2021-10-12 20:28:35 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# -theme-frontend-urano
+Carrossel Full na Home
+    Dysplay on: Página específica
+    Página: CMS homepage
+    Container: Page Top
+
+    Opções do Widget:
+        - Layout: Tela cheia
+
+    
---
+++ src/theme-frontend-urano/registration.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* 
+* Temas para Magento
+* 
+* @category     elOOm
+* @package      Modulo Theme
+* @copyright    Copyright (c) 2021 Ã©lOOm (https://eloom.tech)
+* @version      1.0.0
+* @license      https://opensource.org/licenses/OSL-3.0
+* @license      https://opensource.org/licenses/AFL-3.0
+*
+*/
+declare(strict_types=1);
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::THEME,
+	'frontend/Eloom/urano',
+	__DIR__
+);
