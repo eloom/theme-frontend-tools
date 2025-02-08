fd451800be8ce0199159f90c9ddd11eef93cf365
élOOm <contato@eloom.com.br>
2025-02-07 23:51:38 -0300

    Rename theme

---
+++ src/theme-frontend-fashion/registration.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* 
+* Temas para Magento
+* 
+* @category     elOOm
+* @package      Modulo Theme
+* @copyright    Copyright (c) 2025 Ã©lOOm (https://eloom.com.br)
+* @version      1.0.0
+* @license      https://opensource.org/licenses/OSL-3.0
+* @license      https://opensource.org/licenses/AFL-3.0
+*
+*/
+declare(strict_types=1);
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::THEME,
+	'frontend/Eloom/fashion',
+	__DIR__
+);
